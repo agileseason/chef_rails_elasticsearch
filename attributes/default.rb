59f8408c60b77305c054558cb0df1bd474135b2d
Andrey Sidorov <takandar@gmail.com>
2018-05-13 00:49:46 +0300

    udpate elasticsearch version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['chef_rails_elasticsearch']['version'] = '5.5.9'
+default['chef_rails_elasticsearch']['version'] = '5.6.9'
 default['chef_rails_elasticsearch']['listen'] = '127.0.0.1'
 default['chef_rails_elasticsearch']['allocated_memory'] = nil
